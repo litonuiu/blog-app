2fcd443e15f3a19b125e49ca8f2bc9a4a4ffb6fa
Liton <liton.ust@gmail.com>
2021-04-10 20:34:42 +0600

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { CreateUserComponent } from './user/create-user/create-user.component';
+import { UpdateUserComponent } from './user/update-user/update-user.component';
+import { ViewUserComponent } from './user/view-user/view-user.component';
+import { ListUserComponent } from './user/list-user/list-user.component';
+import { DeleteUserComponent } from './user/delete-user/delete-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    CreateUserComponent,
+    UpdateUserComponent,
+    ViewUserComponent,
+    ListUserComponent,
+    DeleteUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from '../user.service';
+import {User} from '../user';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  public users: User[] | undefined;
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data: User[]) => {
+      console.log(data);
+      this.users = data;
+    });
+  }
+
+}
